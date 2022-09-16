4aa79fba6d936d74126df90db2bf274c3be83826
mikou <mikou.mouad.mail@gmail.com>
2022-09-16 21:38:53 +0200

    test rebase vs merge

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
+<<<<<<< HEAD
 print ('hello mouad one')
+=======
+<<<<<<< HEAD
+print ('hello mouad')
+=======
+print ('hello tata')
+>>>>>>> a929ecc (test rebase vs merge)
+>>>>>>> e12897b (test rebase vs merge)
