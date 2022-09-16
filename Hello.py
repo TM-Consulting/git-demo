02cb184a5b9898c5df8224808d59d256639a0b66
mikou <mikou.mouad.mail@gmail.com>
2022-09-16 21:43:27 +0200

    Ok

--- Hello.py
+++ Hello.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
 print ('hello mouad one')
-=======
-<<<<<<< HEAD
-print ('hello mouad')
-=======
-print ('hello tata')
->>>>>>> a929ecc (test rebase vs merge)
->>>>>>> e12897b (test rebase vs merge)
